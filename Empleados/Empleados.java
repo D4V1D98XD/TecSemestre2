7d33de47e0d36076ae730b6e146bafeb1e1dd4b5
D4V1D98XD <124410459+D4V1D98XD@users.noreply.github.com>
2025-02-18 13:00:46 -0700

    Add files via upload

---
+++ Empleados/Empleados.java
@@ -0,0 +1,71 @@
+package Empleados;
+
+public class Empleados {
+
+	private String nombre;
+	private char genero;
+	private int horasTrabajadas;
+	private int sueldoHora;
+	private int pagoTotal;
+	
+	public  Empleados() {
+		
+	}
+
+	public Empleados(String nombre, char genero, int horasTrabajadas, int sueldoHora, int pagoTotal) {
+		super();
+		this.nombre = nombre;
+		this.genero = genero;
+		this.horasTrabajadas = horasTrabajadas;
+		this.sueldoHora = sueldoHora;
+		this.pagoTotal = pagoTotal;
+	}
+	
+	public String toString() {
+		return nombre + ":" + "\t";
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setGenero(char genero) {
+		this.genero = genero;
+	}
+
+	public void setHorasTrabajadas(int horasTrabajadas) {
+		this.horasTrabajadas = horasTrabajadas;
+	}
+
+	public void setSueldoHora(int sueldoHora) {
+		this.sueldoHora = sueldoHora;
+	}
+
+	public void setPagoTotal(int pagoTotal) {
+		this.pagoTotal = pagoTotal;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public char getGenero() {
+		return genero;
+	}
+
+	public int getHorasTrabajadas() {
+		return horasTrabajadas;
+	}
+
+	public int getSueldoHora() {
+		return sueldoHora;
+	}
+
+	public int getPagoTotal() {
+		return pagoTotal;
+	}
+	
+	
+	
+	
+}
---
+++ Empleados/Empresa.java
@@ -0,0 +1,45 @@
+package Empleados;
+import java.util.*;
+public class Empresa {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cantidad de empleados a consultar: ");
+		int n = scan.nextInt();
+		Empleados[] empleados = new Empleados[n];
+		datosEmpleados(empleados, n);
+
+	}
+	
+	public static void datosEmpleados(Empleados[] c, int n) {
+		Random random = new Random();
+		//names
+		String[] nombres = {"Pepe","Chuy", "Dandi", "Boynas", "Leo", "Julio", "Erik", "Perez", "Adrian", "Harry"};
+		String nombre;
+		//genre
+		char[] genero = {'M', 'F'};
+		char genre;
+		//positions
+		int num = 0, num2 = 0, sueldoHora=0, horasTrabajadas = 0;
+		
+		for(int i=0; i<n; i++) {
+			num = random.nextInt(10);
+			num2 = random.nextInt(2);
+			sueldoHora = random.nextInt(40, 101);
+			horasTrabajadas = random.nextInt(10, 61);
+			nombre = nombres[num];
+			genre = genero[num2];
+			c[i].setNombre(nombre);
+			c[i].setGenero(genre);
+			c[i].setSueldoHora(sueldoHora);
+			c[i].setHorasTrabajadas(horasTrabajadas);
+		}
+	}
+	
+	public static void pagoTotal(Empleados[] c, int n) {
+		int pagoTotal = 0;
+		
+		for(int i=0; i<n; i++) {
+			pagoTotal = c[i].getHorasTrabajadas() * c[i].getPagoTotal();
+		}
+	}
+}
