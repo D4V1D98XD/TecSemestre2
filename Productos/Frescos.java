43b043d94ae787a12ff597161944c2f2637a9fdc
D4V1D98XD <124410459+D4V1D98XD@users.noreply.github.com>
2025-02-26 12:48:07 -0700

    Add files via upload

---
+++ Productos/Frescos.java
@@ -0,0 +1,14 @@
+package Productos;
+
+public class Frescos extends Productos {
+
+	public Frescos() {}
+	
+	public Frescos(String fecha, int lote, String envasado, String pais) {
+		super(fecha, lote, envasado, pais);
+	}
+	
+	public String toString() {
+		
+	}
+}
---
+++ Productos/Productos.java
@@ -0,0 +1,23 @@
+package Productos;
+
+public class Productos {
+
+	private String fecha;
+	private int lote;
+	private String pais;
+	private String envasado;
+	
+	public Productos() {
+		
+	}
+
+	public Productos(String fecha, int lote, String pais, String envasado) {
+		this.fecha = fecha;
+		this.lote = lote;
+		this.pais = pais;
+		this.envasado = envasado;
+	}
+	
+	
+}
+
